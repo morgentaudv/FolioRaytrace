bb82a0c437aa4e568ad92bf1ae7fead6ba4f538f
Morgentau <mgtauneu@gmail.com>
2024-03-10 21:33:48 +0900

    デバッグ関連ロジックをDebugフォルダに

--- FolioRaytrace/World/RenderBuffer.cs
+++ FolioRaytrace/World/RenderBuffer.cs
@@ -5,6 +5,7 @@ using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using FolioRaytrace.Debug;
 
 namespace FolioRaytrace.World
 {
