c27ad4c83204be6e793e4488f660cc0191693e03
Morgentau <mgtauneu@gmail.com>
2024-03-11 23:18:15 +0900

    Textureベースクラス追加

---
+++ FolioRaytrace/Texture/TextureBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolioRaytrace.Texture
+{
+    /// <summary>
+    /// RenderObjectのテクスチャーのベースクラス
+    /// </summary>
+    public interface ITextureBase
+    {
+        /// <summary>
+        /// Value計算の設定
+        /// </summary>
+        struct ValueSetting
+        {
+            public double U;
+            public double V;
+            public RayMath.Vector3 Point;
+        }
+
+        /// <summary>
+        /// setting情報から適切な色の値を返す。
+        /// </summary>
+        public abstract RayMath.Vector3 Value(ValueSetting setting);
+    }
+}
